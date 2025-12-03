3fd5ff4804849f96cae76555f84d2e9a03beb516
Yeaaaaaaaap <disco1212@yonsei.ac.kr>
2025-12-03 23:45:55 +0900

    Save current changes before code replacement

--- backend/app/main.py
+++ backend/app/main.py
@@ -16,8 +16,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
     allow_credentials=True,
-    allow_methods=["*"]
-    ,allow_headers=["*"]
+    allow_methods=["*"],
+    allow_headers=["*"]
 )
 
 # Optionally serve actor images if available
